5bb7e568507b9208a23acc9a85c2a98635776772
ChandanChainani <chainanichan@gmail.com>
2023-03-15 09:07:36 +0530

    Python: Refactor Date Generator

--- python/date_generator.py
+++ python/date_generator.py
@@ -1,4 +1,4 @@
-def get_date(limit):
+def get_date(year, end_range):
     from datetime import datetime
 
     JAN, FEB, MAR, APR, MAY, JUN, JULY, AUG, SEP, OCT, NOV, DEC = (
@@ -7,38 +7,37 @@ def get_date(limit):
         7,  8,  9,
         10, 11, 12
     )
-    TOTAL_DAYS_IN_YEAR = 365
-    day, month, year, days_counter = 1, JAN, 1990, 1
-    i = 0
-    while i < limit:
+    day, month = 1, JAN
+    MONTHS_OF_30_DAYS = {APR, JUN, SEP, NOV}
+    for _ in range(end_range):
         yield datetime(day=day, month=month, year=year).date()
 
         day += 1
-        days_counter += 1
 
         if (
             day > 31
-            or (month == FEB and day > 28)
-            or (day > 30 and month in [APR, JUN, SEP, NOV])
+            or (month == FEB and day > (29 if year % 4 == 0 else 28))
+            or (day > 30 and month in MONTHS_OF_30_DAYS)
         ):
             day = 1
             month += 1
 
         if month > DEC:
             month = JAN
-
-        if days_counter == TOTAL_DAYS_IN_YEAR:
-            days_counter = 0
             year += 1
 
-        i += 1
-
-
-# for date in get_date(365):
+# for date in get_date(2018, 365):
 #     print(date)
 
+TOTAL_DAYS_IN_YEAR = 365
 
-# for idx, date in enumerate(get_date(365 * 3)):
-# for idx, date in enumerate(get_date(365)):
-for idx, date in enumerate(get_date(36)):
-    print("{}: {}".format(str(idx + 1).rjust(3, "0").ljust(4, " "), date))
+IDX = 1
+YEAR = 2018
+for date in get_date(YEAR, TOTAL_DAYS_IN_YEAR * 3 + 1):
+# for idx, date in enumerate(get_date(YEAR, 36)):
+# for idx, date in enumerate(get_date(YEAR, 367)):
+    if IDX > (TOTAL_DAYS_IN_YEAR + (1 if YEAR % 4 == 0 else 0)):
+        YEAR += 1
+        IDX = 1
+    print("{}: {}".format(str(IDX).rjust(3, "0").ljust(4, " "), date))
+    IDX += 1
